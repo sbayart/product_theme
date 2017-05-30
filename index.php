5accd3dc136b8842615ff9977906cafcdc2f5114
salwa <salwaoudad@gmail.com>
2017-05-30 14:27:00 +0200

    add index and style

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Product Launch</title>
+    <link rel="stylesheet" href="<?php echo get_stylesheet_uri()?>" >
+  </head>
+  <body>
+    <header>
+
+    </header>
+    <main>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p>1/6/2017</p>
+    </main>
+
+  </body>
+</html>
