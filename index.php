b2605bcabfb699d89f6a995ea8167bd9e5680089
Sylvie <sylvie.veyres@gmail.com>
2017-05-30 15:55:02 +0200

    add style with picture

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Product Launch</title>
     <link rel="stylesheet" href="<?php echo get_stylesheet_uri()?>" >
+    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
   </head>
   <body>
     <header>
@@ -12,28 +13,30 @@
     </header>
     <main>
         <article class="">
+            <img src="wp-content/themes/product_theme/img/soon.jpg" alt="soon">
             <?php
             foreach ($posts as $post) :
                 setup_postdata( $post );
                 the_title('<h1>','</h1>');
                 the_content('<p>','</p>');
              ?>
-                <p><?php echo get_post_meta($post->ID, 'date', true); ?></p>
+                <p class="date"><?php echo get_post_meta($post->ID, 'date', true); ?></p>
              <?php endforeach;?>
+
+             <form method="get">
+                 <input type="text" name="first_name" value="" placeholder="First Name">
+                 <input type="text" name="last_name" value="" placeholder="Last Name">
+                 <input type="mail" name="email" value="" placeholder="Email">
+                 <?php
+                        $wpdb->insert(
+                             'form',  array(    'first_name' => $_GET['first_name'],
+                                                'last_name' => $_GET['last_name'],
+                                                'email' => $_GET['email']
+                                         ));
+                      ?>
+                     <button type="submit" name="button">Recevoir la newsletter</button>
+             </form>
          </article>
-         <form method="get">
-             <input type="text" name="first_name" value="" placeholder="First Name">
-             <input type="text" name="last_name" value="" placeholder="Last Name">
-             <input type="mail" name="email" value="" placeholder="Email">
-             <?php
-                     $wpdb->insert(
-                         'form',  array(    'first_name' => $_GET['first_name'],
-                                            'last_name' => $_GET['last_name'],
-                                            'email' => $_GET['email']
-                                     ));
-                  ?>
-                 <button type="submit" name="button">Recevoir la newsletter</button>
-         </form>
     </main>
 
   </body>
